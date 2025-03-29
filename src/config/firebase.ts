7f12508ab1ab236405fc03f7f54bbd64004c5dc6
mimansha-swarup <mimanshaswarup@gmail.com>
2025-03-29 23:22:07 +0530

    chore: comment out Firebase service account configuration for debugging

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,16 +1,16 @@
 import firebase from "firebase-admin";
-import serviceAccountJSON from "../../service.json";
+// import serviceAccountJSON from "../../service.json";
 
 let serviceAccount: firebase.ServiceAccount;
-if (process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
+// if (process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
   const buff = Buffer.from(
     process.env.FIREBASE_SERVICE_ACCOUNT_BASE64,
     "base64"
   );
   serviceAccount = JSON.parse(buff.toString("utf-8"));
-} else {
-  serviceAccount = serviceAccountJSON as firebase.ServiceAccount;
-}
+// } else {
+//   serviceAccount = serviceAccountJSON as firebase.ServiceAccount;
+// }
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
 });
