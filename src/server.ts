2ace58ffbd3d7f0245333dd0f3cbdea0a7cd7581
mimansha-swarup <mimanshaswarup@gmail.com>
2025-06-16 09:31:38 +0530

    refactor: remove commented-out local server listening logic in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,5 @@
 import app from "./app";
 import serverless from "serverless-http";
 
-// if (process.env.NODE_ENV === "LOCAL") {
-//   const port = 3000;
-//   app.listen(port, () => {
-//     console.log(`The server is running at ${port}`);
-//   });
-// }
-
-// export default app;
 
 export const handler = serverless(app);
