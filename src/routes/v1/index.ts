4dfcaebdf97d0074e7600ac5a193cba0ee8b8458
mimansha-swarup <mimanshaswarup@gmail.com>
2025-10-17 17:39:02 +0530

    render hack

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import userRouter from "./user.route.js";
 import contentRouter from "./content.route.js";
 
@@ -6,5 +6,8 @@ const router = Router();
 
 router.use("/user", userRouter);
 router.use("/content", contentRouter);
+router.get("/health", (_req: Request, res: Response) => {
+  res.sendSuccess({ statusCode: 200, message: "ok" });
+});
 
 export default router;
